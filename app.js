a2603f6700f871cf4584f33ac0cecad637351138
darsuabasi <uduakabasiabasiurua@pursuit.org>
2020-01-25 12:52:44 -0500

    server is set up on app

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors")
+const bodyParser = require("body-parser")
+
+const port = 3000 
+
+const app = express(); 
+app.use(cors()); 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+    console.log("Server is running " + port)
+})
