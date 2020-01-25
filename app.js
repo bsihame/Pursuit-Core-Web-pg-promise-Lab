2ede031e441e11661d50544b079efcc4561da55c
darsuabasi <uduakabasiabasiurua@pursuit.org>
2020-01-25 13:08:02 -0500

    set up files

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-const express = require("express");
-const cors = require("cors")
-const bodyParser = require("body-parser")
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
 
-const port = 3000 
+// const usersRouter = require('./routes/users');
+// const postsRouter = require('./routes/posts');
 
-const app = express(); 
-app.use(cors()); 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+// app.use('/users', usersRouter);
+// app.use('/posts', postsRouter);
 
 app.listen(port, () => {
-    console.log("Server is running " + port)
-})
+  console.log(`Listening on port ${port}!`);
+});
