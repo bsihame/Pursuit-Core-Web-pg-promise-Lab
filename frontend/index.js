c808df1d5e69f52878d2c735078549a29531fd54
bsihame <sihamebazi@pursuit.org>
2020-01-25 22:40:15 -0500

    creat frontend file

--- frontend/index.js
+++ frontend/index.js
@@ -4,22 +4,33 @@ document.addEventListener('DOMContentLoaded', () => {
     form.addEventListener('submit', addUserFormSubmitted);
 });
 
-async function loadUsers() {
+const loadUsers = async() =>{
+    try{
     const usersList = document.querySelector('#usersList');
     usersList.innerHTML = "";
     const response = await axios.get(`http://localhost:3000/users/all`);
     response.data.users.forEach((user) => {
+        //debugger
         let listItem = document.createElement("li");
-        listItem.innerText = `${user.firstname} ${user.lastname}, age ${user.age}`;
+        listItem.innerText = `${user.firstname} ${user.lastname}, ${user.age}`;
         usersList.appendChild(listItem);
     });
+    }catch (err) {
+        console.log(err)
+    }
 }
 
-async function addUserFormSubmitted(event) {
-    event.preventDefault();    
+const addUserFormSubmitted = async(event) =>{
+    event.preventDefault();   
+    try{
     const firstname = document.querySelector('#firstNameInput').value;
     const lastname = document.querySelector('#lastNameInput').value;
     const age = document.querySelector('#ageInput').value;
-    let response = await axios.post(`http://localhost:3000/users/register`, { firstname, lastname, age });
+    // debugger
+    let response = await axios.post(`http://localhost:3000/users/register`, {firstname, lastname, age});
     loadUsers();
+    console.log(response)
+    } catch (err){
+        console.log(err)
+    }
 }
